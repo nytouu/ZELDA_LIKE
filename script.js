a35d460bf7e531fb2427480a148d91fd43825369
nytouu <capitaine7312@gmail.com>
2023-03-20 14:20:50 +0100

    fix movement(somewhat), fix hitbox when running, added layers

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var config =
 {
     type: Phaser.AUTO,
-    width: 1280, height: 720,
+    width: 1024, height: 720,
     physics:
     {
         default: 'arcade',
         arcade:
         {
+            gravity: { y: 0 },
             debug: true
         }
     },
@@ -27,15 +28,17 @@ new Phaser.Game(config);
 function preload()
 {
     this.load.image('background', 'assets/background.png');
-    this.load.image('city_tiles', 'assets/city.png')
+    this.load.image('player_shadow', 'assets/player_shadow.png');
+    this.load.image('city_above', 'assets/city_above_player.png')
+    this.load.image('city_under', 'assets/city_under_player.png')
     this.load.spritesheet('player_idle_back','assets/player_idle_back.png',
-                { frameWidth: 16, frameHeight: 28 });
+                { frameWidth: 32, frameHeight: 32 });
     this.load.spritesheet('player_idle_front','assets/player_idle_front.png',
-                { frameWidth: 16, frameHeight: 28 });
+                { frameWidth: 32, frameHeight: 32 });
     this.load.spritesheet('player_idle_right','assets/player_idle_right.png',
-                { frameWidth: 16, frameHeight: 28 });
+                { frameWidth: 32, frameHeight: 32 });
     this.load.spritesheet('player_idle_left','assets/player_idle_left.png',
-                { frameWidth: 16, frameHeight: 28 });
+                { frameWidth: 32, frameHeight: 32 });
     this.load.spritesheet('player_run_back','assets/player_run_back.png',
                 { frameWidth: 32, frameHeight: 32 });
     this.load.spritesheet('player_run_front','assets/player_run_front.png',
@@ -59,7 +62,7 @@ var game_over = false;
 var controller = false;
 var physics;
 
-const SPEED = 100;
+const SPEED = 80;
 const TILE_SIZE = 16;
 const MAP_SIZE_X = 272;
 const MAP_SIZE_Y = 384;
@@ -71,24 +74,40 @@ function create()
     this.add.image(MAP_SIZE_X / 2, MAP_SIZE_Y / 2, 'background');
 
     const level_map = this.add.tilemap("map");
-    const tileset = level_map.addTilesetImage(
-        "city_map",
-        "city_tiles"
+    const tiles_under = level_map.addTilesetImage(
+        "city_under",
+        "city_under"
     );
-    const city_map = level_map.createLayer(
-        "objects",
-        tileset
+    const tiles_above = level_map.addTilesetImage(
+        "city_above",
+        "city_above"
+    );
+    const city_map_under = level_map.createLayer(
+        "under",
+        tiles_under
+    );
+    const city_map_above = level_map.createLayer(
+        "above",
+        tiles_above
     );
 
     player = physics.add.sprite(120, 340, 'player_idle_front');
+    shadow = physics.add.sprite(120, 340, 'player_shadow');
+    player.setSize(8,14).setOffset(12,16);
+    // hitbox = physics.add.sprite(120, 340, 'hitbox');
+    // hitbox.setSize(8,24).setOffset(4,0);
     player.can_get_hit = true;
 	player.can_dash = true;
+	player.is_dashing = false;
     player.hp = 5;
 
-    city_map.setCollisionByProperty({ isSolid: true });
+    const layer = this.add.layer();
+    layer.add([ city_map_under, shadow, player, city_map_above ])
+
+    city_map_above.setCollisionByProperty({ isSolid: true });
     player.setCollideWorldBounds(true);
 
-    physics.add.collider(player, city_map);
+    physics.add.collider(player, city_map_above);
 
     physics.world.setBounds(0, 0, MAP_SIZE_X, MAP_SIZE_Y);
     this.cameras.main.setBounds(0, 0, MAP_SIZE_X, MAP_SIZE_Y);
@@ -159,32 +178,40 @@ function create()
 
 function update()
 {
+    shadow.x = player.x;
+    shadow.y = player.y;
+
     if (game_over){return;}
+
+    // console.log(player.body.velocity)
 
     if (cursors.left.isDown || controller.left)
     {
-        player.setVelocityX(-SPEED);
+        player.body.setVelocityX(-SPEED);
         player.anims.play('run_left', true);
         player.direction = "left"
     }
     else if (cursors.right.isDown || controller.right)
     {
-        player.setVelocityX(SPEED);
+        player.body.setVelocityX(SPEED);
         player.anims.play('run_right', true);
         player.direction = "right"
     }
+
     if (cursors.up.isDown || controller.up)
     {
-        player.setVelocityY(-SPEED);
+        player.body.setVelocityY(-SPEED);
         player.anims.play('run_back', true);
         player.direction = "back"
     }
     else if (cursors.down.isDown || controller.down)
     {
-        player.setVelocityY(SPEED);
+        player.body.setVelocityY(SPEED);
         player.anims.play('run_front', true);
         player.direction = "front"
     }
+    player.body.velocity.normalize().scale(SPEED);
+
     if (cursors.up.isUp && cursors.down.isUp && cursors.left.isUp && cursors.right.isUp)
     {
         player.setVelocity(0);
@@ -206,7 +233,7 @@ function update()
     }
 
     if (player.can_dash && (cursors.space.isDown || controller.A))
-        player_dash();
+        player_dash(player.direction);
 }
 
 
@@ -215,9 +242,14 @@ function lock_input()
     input_locked = false;
 }
 
-function cd_dash()
+function cd_can_dash()
 {
     player.can_dash = true;
+}
+
+function cd_dash()
+{
+    player.is_dashing = false;
 }
 
 function cd_can_get_hit()
@@ -229,10 +261,13 @@ function cd_can_get_hit()
         player.setTint(0xffffff);
 }
 
-function player_dash()
+function player_dash(direction)
 {
+    console.log(direction)
+    if (direction == "left"){}
     player.can_dash = false;
-    setTimeout(cd_dash, 1000);
+    setTimeout(cd_dash, 200);
+    setTimeout(cd_can_dash, 1000);
 
 	console.log("dash")
 }
@@ -243,46 +278,6 @@ function kill_player()
     game_over = true;
     player.setTint(0xff0000);
     physics.pause();
-}
-
-function hit_bat1()
-{
-    if (player.x < bat1.x && bat1.alive && bat1.can_get_hit)
-    {
-        bat1.alive = false;
-
-        bat1.setTint(0xff0000);
-        this.tweens.add({
-            targets: bat1,
-            alpha: 0,
-            duration: 2000,
-            ease: 'Power2'
-        }, this);
-    }
-    else if (bat1.alive)
-    {
-        damage_player();
-    }
-}
-
-function hit_bat2()
-{
-    if (player.x < bat2.x && bat2.alive && bat2.can_get_hit)
-    {
-        bat2.alive = false;
-
-        bat2.setTint(0xff0000);
-        this.tweens.add({
-            targets: bat2,
-            alpha: 0,
-            duration: 2000,
-            ease: 'Power2'
-        }, this);
-    }
-    else if (bat2.alive)
-    {
-        damage_player();
-    }
 }
 
 function damage_player()
---
+++ tiled/city_above.tsx
@@ -0,0 +1,464 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="city_above" tilewidth="16" tileheight="16" tilecount="408" columns="17">
+ <image source="city_above_player.png" width="272" height="384"/>
+ <tile id="8">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="45">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="103">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="120">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="154">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="171">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="177">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="185">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="188">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="194">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="195">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="196">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="205">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="219">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="222">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="236">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="239">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="253">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="256">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="270">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="273">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="287">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="290">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="304">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="305">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="307">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="308">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="309">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="310">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="311">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="312">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="313">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="316">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="317">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="318">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="319">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="320">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="321">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="329">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="334">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="335">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="336">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="337">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="346">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="351">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="353">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="354">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="363">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="368">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="369">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="370">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="371">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="380">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="381">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="382">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="383">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="384">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="385">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
