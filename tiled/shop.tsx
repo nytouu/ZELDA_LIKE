5a185f0d206bd36d9b5a51e4b72712351733bcb2
nytouu <capitaine7312@gmail.com>
2023-03-30 12:55:50 +0200

    update shop map

--- tiled/shop.tsx
+++ tiled/shop.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="shop" tilewidth="16" tileheight="16" tilecount="30" columns="6">
- <image source="../assets/shop.png" width="96" height="80"/>
+<tileset version="1.10" tiledversion="1.10.0" name="shop" tilewidth="16" tileheight="16" tilecount="35" columns="7">
+ <image source="../assets/shop.png" width="112" height="80"/>
  <tile id="0">
   <properties>
    <property name="isSolid" type="bool" value="true"/>
@@ -31,17 +31,12 @@
    <property name="isSolid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="6">
+ <tile id="7">
   <properties>
    <property name="isSolid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="8">
-  <properties>
-   <property name="isSolid" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="11">
+ <tile id="9">
   <properties>
    <property name="isSolid" type="bool" value="true"/>
   </properties>
@@ -56,7 +51,7 @@
    <property name="isSolid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="18">
+ <tile id="16">
   <properties>
    <property name="isSolid" type="bool" value="true"/>
   </properties>
@@ -66,27 +61,17 @@
    <property name="isSolid" type="bool" value="true"/>
   </properties>
  </tile>
+ <tile id="21">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="23">
   <properties>
    <property name="isSolid" type="bool" value="true"/>
   </properties>
  </tile>
- <tile id="24">
-  <properties>
-   <property name="isSolid" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="25">
-  <properties>
-   <property name="isSolid" type="bool" value="true"/>
-  </properties>
- </tile>
  <tile id="26">
-  <properties>
-   <property name="isSolid" type="bool" value="true"/>
-  </properties>
- </tile>
- <tile id="27">
   <properties>
    <property name="isSolid" type="bool" value="true"/>
   </properties>
@@ -97,6 +82,26 @@
   </properties>
  </tile>
  <tile id="29">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33">
   <properties>
    <property name="isSolid" type="bool" value="true"/>
   </properties>
