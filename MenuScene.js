a4ece8c9fa13e74936dbf3e7fe0ee850bfd895e5
nytouu <capitaine7312@gmail.com>
2023-04-03 00:16:07 +0200

    cool menu

--- MenuScene.js
+++ MenuScene.js
@@ -1,4 +1,5 @@
-const BG_SIZE = 512;
+const BG_SIZE_X = 1280 ;
+const BG_SIZE_Y = 720 ;
 var mx;
 var my;
 
@@ -15,31 +16,40 @@ export class MenuScene extends Phaser.Scene{
 		this.cameras.main.fadeIn(400, 0, 0, 0);
 	}
     preload(){
-        this.load.image('background', 'assets/background.png');
-        this.load.spritesheet('button','assets/play_button.png',
-                    { frameWidth: 128, frameHeight: 64 });
+        this.load.image('background_menu', 'assets/menu/background.png');
+
+        this.load.image('name', 'assets/menu/name.png');
+        this.load.image('text', 'assets/menu/text.png');
+
+        this.load.image('losang', 'assets/menu/losang.png');
+		this.load.image('square', 'assets/menu/square.png');
+		this.load.image('square2', 'assets/menu/square2.png');
+        this.load.image('circle', 'assets/menu/circle.png');
+
+        this.load.image('dark1', 'assets/menu/dark1.png');
+        this.load.image('dark2', 'assets/menu/dark2.png');
+        this.load.image('dark3', 'assets/menu/dark3.png');
+        this.load.image('dark4', 'assets/menu/dark4.png');
     }
     create(){
-        this.background = this.add.image(BG_SIZE, BG_SIZE / 1.5, 'background');
-        this.button = this.add.sprite( BG_SIZE,BG_SIZE / 1.5, 'button');
+        this.background_menu = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'background_menu');
+        this.name = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'name');
+        this.text = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'text');
+        this.losang = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'losang');
+        this.square = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'square');
+        this.square2 = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'square2');
+        this.circle = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'circle');
+
+        this.dark1 = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'dark1');
+        this.dark2 = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'dark2');
+        this.dark3 = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'dark3');
+        this.dark4 = this.add.image(BG_SIZE_X / 1.34, BG_SIZE_Y / 1.34, 'dark4');
 
         const layer = this.add.layer();
-        layer.add([ this.background, this.button ])
+        layer.add([ this.background_menu, this.dark4, this.dark3, this.dark2, this.dark1, 
+			this.circle, this.square2, this.square, this.losang, this.text, this.name ])
 
-        this.cameras.main.setZoom(1);
-
-        this.anims.create({
-            key: 'button_idle',
-            frames: [ { key: 'button', frame: 0} ],
-            frameRate: 1,
-            repeat: 0
-        });
-        this.anims.create({
-            key: 'button_hover',
-            frames: [ { key: 'button', frame: 1} ],
-            frameRate: 1,
-            repeat: 0
-        });
+        this.cameras.main.setZoom(1.50);
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -53,23 +63,46 @@ export class MenuScene extends Phaser.Scene{
         mx = this.input.mousePointer.x;
         my = this.input.mousePointer.y;
 
-        this.background.x = (((BG_SIZE) * (mx / BG_SIZE)  * 0.01) + (BG_SIZE));
-        this.background.y = (((BG_SIZE) * (my / BG_SIZE)) * 0.01) + (BG_SIZE / 1.5);
+        this.background_menu.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.003) + (BG_SIZE_X) / 1.34);
+        this.background_menu.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.003) + (BG_SIZE_Y / 1.34);
 
-        if (mx >= 320 && mx <= 700
-         && my >= 210 && my <= 400)
-        {
-            this.button.anims.play('button_hover');
-            if (this.click == true)
-                this.cameras.main.fadeOut(400, 0, 0, 0);
-                this.time.delayedCall(500, () => {
-					this.scene.start('RoomScene');
-                })
-        }
-        else
-        {
-            this.button.anims.play('button_idle');
-        }
+        this.name.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.02) + (BG_SIZE_X) / 1.34);
+        this.name.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.02) + (BG_SIZE_Y / 1.34);
+
+        this.text.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.002) + (BG_SIZE_X) / 1.34);
+        this.text.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.002) + (BG_SIZE_Y / 1.34);
+
+        this.losang.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X / 1.34)  * 0.03) + (BG_SIZE_X) / 1.34);
+        this.losang.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y / 1.34)) * 0.03) + (BG_SIZE_Y / 1.34);
+
+        this.square.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.04) + (BG_SIZE_X) / 1.34);
+        this.square.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.04) + (BG_SIZE_Y / 1.34);
+
+        this.square2.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.05) + (BG_SIZE_X) / 1.34);
+        this.square2.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.05) + (BG_SIZE_Y / 1.34);
+
+        this.circle.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.06) + (BG_SIZE_X) / 1.34);
+        this.circle.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.06) + (BG_SIZE_Y / 1.34);
+
+        this.dark1.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.02) + (BG_SIZE_X) / 1.34);
+        this.dark1.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.02) + (BG_SIZE_Y / 1.34);
+
+        this.dark2.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.03) + (BG_SIZE_X) / 1.34);
+        this.dark2.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.03) + (BG_SIZE_Y / 1.34);
+
+        this.dark3.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.04) + (BG_SIZE_X) / 1.34);
+        this.dark3.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.04) + (BG_SIZE_Y / 1.34);
+
+        this.dark4.x = (((BG_SIZE_X / 1.34) * (mx / BG_SIZE_X/ 1.34)  * 0.04) + (BG_SIZE_X) / 1.34);
+        this.dark4.y = (((BG_SIZE_Y / 1.34) * (my / BG_SIZE_Y/ 1.34)) * 0.04) + (BG_SIZE_Y / 1.34);
+
+		if (this.click == true)
+		{
+			this.cameras.main.fadeOut(900, 0, 0, 0);
+			this.time.delayedCall(1000, () => {
+				this.scene.start('RoomScene');
+			})
+		}
         this.click = false;
     }
 };
