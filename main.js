750a50ad7938b84a17e0f85e811785a7e296b9d7
nytouu <capitaine7312@gmail.com>
2023-04-11 09:27:49 +0200

    electron app

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.setMenuBarVisibility(false);
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow)
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
